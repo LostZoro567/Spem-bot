a4a7d2bc67d833189f44b936436a62fe146f942c
LostZoro567 <149526552+LostZoro567@users.noreply.github.com>
2025-04-21 15:01:58 +0530

    Update userbot_keyboard.py

--- userbot_keyboard.py
+++ userbot_keyboard.py
@@ -1,8 +1,8 @@
 from telethon import TelegramClient, events, Button
 
-api_id = 123456
-api_hash = 'your_api_hash_here'
-session_name = 'your_session'
+api_id = 23097454
+api_hash = 'c244cdd380d02f0d51eca49df46db852'
+session_name = 'userbot_session'
 
 client = TelegramClient(session_name, api_id, api_hash)
 
